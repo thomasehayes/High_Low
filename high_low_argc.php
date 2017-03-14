2d1b1411fc960f785bf5d44536525e189c5d161c
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-14 15:12:29 -0500

    Added more user friendliness by adding new comments if non numeric values were entered and what range to look between.

--- high_low_argc.php
+++ high_low_argc.php
@@ -6,14 +6,14 @@ if ($argc >= 3 && is_numeric($argv[1]) && is_numeric($argv[2])){
 	$min = $argv[1];
 	$max = $argv[2];
 } else if ($argc >= 3 && (!is_numeric($argv[1]) || !is_numeric($argv[2]))) {
-	fwrite(STDOUT, "Add min values" . PHP_EOL);
+	fwrite(STDOUT, "Add numeric min value" . PHP_EOL);
 	$min = trim(fgets(STDIN));
-	fwrite(STDOUT, "Add max values" . PHP_EOL);
+	fwrite(STDOUT, "Add numeric max value" . PHP_EOL);
 	$max = trim(fgets(STDIN));
 }
 $random = rand($min, $max);
 
-fwrite(STDOUT, "Guess a number" . PHP_EOL);
+fwrite(STDOUT, "Guess a number between $min and $max" . PHP_EOL);
 $playerGuess = trim(fgets(STDIN));
 
 do {
