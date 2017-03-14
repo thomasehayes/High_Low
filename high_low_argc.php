0c7e80bd306a6a15b2d1d151666b33fc1c373d56
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-14 14:00:51 -0500

    Created arguments that checks if it is numeric

---
+++ high_low_argc.php
@@ -0,0 +1,27 @@
+<?php 
+$random = rand(1, 100);
+
+if ($argc < 3) {
+	$random = rand(1, 100);
+} else if (!is_numeric($argv[1]) && !is_numeric($argv[2])){
+	$random = rand($argv[1], $argv[2]);
+}
+
+
+
+
+fwrite(STDOUT, "Guess a number" . PHP_EOL);
+$playerGuess = trim(fgets(STDIN));
+
+do {
+	if ($playerGuess > $random) {
+		echo "LOWER" . PHP_EOL;
+		$playerGuess = trim(fgets(STDIN));
+	} else if ($playerGuess < $random) {
+		echo "HIGHER" . PHP_EOL;
+		$playerGuess = trim(fgets(STDIN));
+	}
+} while ($playerGuess != $random);
+	echo "GOOD GUESS". PHP_EOL;
+
+?>
